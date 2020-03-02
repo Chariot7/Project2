2340dbac5d955edc08bea57f5c7060d12e6b61b7
Brennan Lowe <brennanlowe@Brennans-MacBook-Pro.local>
2020-03-02 14:09:16 -0800

    begin OAuth

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/wishes',
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    }
+);
+
+// shortcut to mongoose.connection object
+const db = mongoose.connection;
+
+db.on('connected', function () {
+    console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
+});
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session')
+
+require('dotenv').config();
+require('./config/database');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,8 +23,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'WishpoolRocks',
+  resave: false,
+  saveUninitialized: true
+}));
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
