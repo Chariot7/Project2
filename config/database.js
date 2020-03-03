7c6d103d67693fed15ddab2a9232a6936d41d859
Brennan Lowe <brennanlowe@Brennans-MacBook-Pro.local>
2020-03-02 20:43:39 -0800

    Oauth, atlas complete

--- config/database.js
+++ config/database.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/wishes',
+mongoose.connect(process.env.DATABASE_URL,
     {
         useNewUrlParser: true,
         useCreateIndex: true,
         useUnifiedTopology: true,
     }
 );
+// mongoose.connect('mongodb://localhost/users',
+//     {
+//         useNewUrlParser: true,
+//         useCreateIndex: true,
+//         useUnifiedTopology: true,
+//     }
+// );
 
 // shortcut to mongoose.connection object
 const db = mongoose.connection;
@@ -14,3 +21,5 @@ const db = mongoose.connection;
 db.on('connected', function () {
     console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
 });
+
+module.exports = mongoose;
